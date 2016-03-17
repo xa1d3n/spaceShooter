e0cacff13bafb52bce10d8de034d6eadd5ec5cf4
Aldin Fajic <afajic@hotmail.com>
2016-03-16 23:07:32 -0400

    Resource files, enemy ship, player ship and bullets implemented

--- SpaceShooter/GameScene.swift
+++ SpaceShooter/GameScene.swift
@@ -9,14 +9,53 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var Player = SKSpriteNode(imageNamed: "PlayerShip")
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
-        self.addChild(myLabel)
+        Player.position = CGPointMake(self.size.width / 2, self.size.height / 9)
+        
+        // spawn bullets every 0.2 seconds
+        _ = NSTimer.scheduledTimerWithTimeInterval(0.2, target: self, selector: "spawnBullets", userInfo: nil, repeats: true)
+        
+        _ = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "spawnEnemies", userInfo: nil, repeats: true)
+
+        self.addChild(Player)
+    }
+    
+    func spawnEnemies() {
+        // create enemy sprite
+        let enemy = SKSpriteNode(imageNamed: "EnemyShip")
+        // randomly place the images accross x axis
+        let minValue = self.size.width / 8
+        let maxValue = self.size.width - 20
+        let spawnPoint = UInt32(maxValue - minValue)
+        // spawn at random position along x axis
+        enemy.position = CGPointMake(CGFloat(arc4random_uniform(spawnPoint)), self.size.height)
+        
+        // move down
+        let animation = SKAction.moveToY(-70, duration: 3.0)
+        let runForever = SKAction.repeatActionForever(animation)
+        enemy.runAction(runForever)
+        
+        addChild(enemy)
+    }
+    
+    func spawnBullets() {
+        // create the bullet
+        let bullet = SKSpriteNode(imageNamed: "Bullet")
+        // set behind ship
+        bullet.zPosition = -5
+        // set position right behind ship
+        bullet.position = CGPointMake(Player.position.x, Player.position.y)
+        // animate so it moves up and disspears over
+        let animation = SKAction.moveToY(self.size.height + 30, duration: 1.0)
+        // run forever
+        let runForever = SKAction.repeatActionForever(animation)
+        bullet.runAction(runForever)
+        addChild(bullet)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -24,7 +63,16 @@ class GameScene: SKScene {
         
         for touch in touches {
             let location = touch.locationInNode(self)
-            
+            // move player to touch location
+            Player.position.x = location.x
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            // move player to touch location
+            Player.position.x = location.x
         }
     }
    
